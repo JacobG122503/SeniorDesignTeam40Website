063447dc86e62b61f73ef84eeea1bee0e214ea8b
Jacob Garcia <jacobgar@iastate.edu>
2025-09-13 16:45:42 -0500

    Even more setup

--- Team40Website/Program.cs
+++ Team40Website/Program.cs
@@ -2,13 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Team40Website;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
