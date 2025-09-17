7075d85e556ab3de7e7eff07f7435f890c1105cf
Jacob Garcia <jacobgar@iastate.edu>
2025-09-16 22:13:23 -0500

    added team member

--- Team40Website/Models/BuildInfo.cs
+++ Team40Website/Models/BuildInfo.cs
@@ -2,6 +2,6 @@ namespace Team40Website.Models
 {
     public static class BuildInfo
     {
-        public const string BuildTimestamp = "9/16/2025 5:35 PM";
+        public const string BuildTimestamp = "9/16/2025 10:02 PM";
     }
 }
