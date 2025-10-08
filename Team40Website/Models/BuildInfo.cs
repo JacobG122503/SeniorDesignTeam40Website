af9b4613d3c93912eadc4f047366885f0bbe0cc2
jacobg <jaybob1225@gmail.com>
2025-10-08 12:10:09 -0500

    Week 2 report added

--- Team40Website/Models/BuildInfo.cs
+++ Team40Website/Models/BuildInfo.cs
@@ -2,6 +2,6 @@ namespace Team40Website.Models
 {
 public static class BuildInfo
 {
-public const string BuildTimestamp = "10/7/2025 2:53 PM";
+public const string BuildTimestamp = "10/8/2025 12:09 PM";
 }
 }
