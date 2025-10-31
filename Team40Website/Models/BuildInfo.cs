7cecbe629258d37591de4ba4699b372245edd083
jacobg <jaybob1225@gmail.com>
2025-10-31 13:51:27 -0500

    Add team resources folder

--- Team40Website/Models/BuildInfo.cs
+++ Team40Website/Models/BuildInfo.cs
@@ -2,6 +2,6 @@ namespace Team40Website.Models
 {
 public static class BuildInfo
 {
-public const string BuildTimestamp = "10/29/2025 9:29 AM";
+public const string BuildTimestamp = "10/31/2025 1:51 PM";
 }
 }
