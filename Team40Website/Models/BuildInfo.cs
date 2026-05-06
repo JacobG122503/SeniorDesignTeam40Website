2e48af5219b3fff2f4a152cdf1dbf848f25783aa
jacobg <jaybob1225@gmail.com>
2026-05-06 08:11:08 -0500

    slides updater

--- Team40Website/Models/BuildInfo.cs
+++ Team40Website/Models/BuildInfo.cs
@@ -2,6 +2,6 @@ namespace Team40Website.Models
 {
 public static class BuildInfo
 {
-public const string BuildTimestamp = "5/5/2026 4:57 PM";
+public const string BuildTimestamp = "5/6/2026 8:06 AM";
 }
 }
